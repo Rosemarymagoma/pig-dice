18a6b2a63917f0456a1241a48b5532edce5a3166
rosemarymagoma99@gmail.com <rosemarymagoma99@gmail.com>
2018-09-28 18:37:04 +0300

    add sandybrown color to styless.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,128 +1,88 @@
-//
-var playerOne = "";
-var playerTwo = "";
-
-var throwDice = function() {
-  return Math.floor(6 * Math.random()) + 1;
-}
-
-function Player(turn) {
-  this.roll = 0;
-  this.tempPoints = 0;
-  this.totalPoints = 0;
-  this.turn = turn;
-  this.playerNumber;
-}
-
-// checking for 1
-Player.prototype.rollone = function() {
-  if (this.roll === 1) {
-    this.tempPoints = 0;
-    alert("Sorry " + this.player + ", you rolled a 1! Your turn is over!")
-    // this.changeturn();
-  } else {
-    this.tempPoints += this.roll;
+//business logic
+function gameplayer() {
+  this.turnScore = 0;
+  this.totalScore = 0;
+  this.winningScore = 100;
+  this.roll = function() {
+    return Math.floor(Math.random() * 6) + 1;
   }
 }
 
-// hold
-Player.prototype.hold = function() {
-  this.totalPoints += this.trialPoints;
-  this.tempPoints = 0;
-  // this.changeturn();
-  alert(this.playerNumber + ", your turn is over, pass the mouse!");
-}
 
-// // changing turn
-// Player.prototype.changeturn = function () {
-//   if (this.roll ===1) {
-//     this.turn = false;
-//   } else {
-//     this.turn = true;
-//   }
-// }
-// check for 100
-Player.prototype.winnerCheck = function() {
-  if (this.totalPoints >= 100) {
-    alert(this.playerNumber + " You are the winner!");
-  }
-}
 
-Player.prototype.newGame = function() {
-  //debugger;
-  this.roll = 0;
-  this.tempPoints = 0;
-  this.totalPoints = 0;
-  this.playerNumber = "";
-}
-
-var clearValues = function() {
-  $("#firstPlayer").val("");
-  $("#secondPlayer").val("");
-}
-
-// User Interface
+//UI logic
 $(document).ready(function() {
+  $("#button").click(function() {
+    $(".rule").toggle()
+  });
+  $("#game-container").hide();
 
-  $("button#start").click(function(event) {
-    playerOne = new Player(true);
-    playerTwo = new Player(false);
-    $(".playing").show();
-    $(".begin").hide();
+  $("#start-game-button").click(function(event) {
+    event.preventDefault();
 
-    var firstPlayer = $(".firstPlayer").val();
-    $("#firstPlayer").text(firstPlayer);
+    player1 = $("#player1Name").val();
+    player2 = $("#player2Name").val();
+    $("#playerinfo").hide();
+    $("#game-container").show();
+    $(".rule").hide()
 
-    var secondPlayer = $(".secondPlayer").val();
-    $("#secondPlayer").text(secondPlayer);
 
-     PlayerOne.firstPlayer = firstPlayer;
-    PlayerTwo.secondPlayer = firstPlayer;
+    // display input
+    $("#gameStartPlayer1Name").text(player1);
+    $("#gameStartPlayer2Name").text(player2);
+
+    player1Stats = new gameplayer();
+    player2Stats = new gameplayer();
 
   });
-  $("button#new-game").click(function(event) {
-    $(".player-console").hide();
-    clearValues();
-     playerOne.newGame();
-     playerTwo.newGame();
-    $("#timesTotalOne").empty();
-    $("#totalPointsOne").empty();
-    $("#dieRollOne").empty();
-    $("#timesTotalTwo").empty();
-    $("#totalPointsTwo").empty();
-    $("#dieRollTwo").empty();
+  //player1roll
+  $("#roll-dice-button1").click(function() {
+    var rolled = player1Stats.roll();
+    player1Stats.turnScore += rolled;
+    if (rolled == 1) {
+      player1Stats.turnScore = 0;
+      $("#stats-turn-score1").text(player1Stats.turnScore);
+      $("#play1").hide()
+      $("#play2").show()
 
-    $(".begin").show();
+    }
+
+    $("#stats-turn-score1").text(player1Stats.turnScore);
+    $("#stats-player-1-total-score1").text(player1Stats.totalScore);
+
+  });
+  $("#hold-button1").click(function() {
+    player1Stats.totalScore += player1Stats.turnScore;
+    player1Stats.turnScore = 0;
+    $("#stats-player-1-total-score").text(player1Stats.totalScore);
+    $("#stats-turn-score1").text(player1Stats.turnScore);
+    $("#play1").hide()
+    $("#play2").show()
+
+  });
+  //player2 roll
+  $("#roll-dice-button2").click(function() {
+    var rolled = player2Stats.roll();
+    player2Stats.turnScore += rolled;
+    if (rolled == 1) {
+      player2Stats.turnScore = 0;
+      $("#stats-turn-score2").text(player2Stats.turnScore);
+      $("#play2").hide()
+      $("#play1").show()
+
+    }
+
+    $("#stats-turn-score2").text(player2Stats.turnScore);
+
+  });
+  $("#hold-button2").click(function() {
+    player2Stats.totalScore += player2Stats.turnScore;
+    player2Stats.turnScore = 0;
+    $("#stats-player-2-total-score").text(player2Stats.totalScore);
+    $("#stats-turn-score2").text(player2Stats.turnScore);
+    $("#play2").hide()
+    $("#play1").show()
   });
 
-  $("button#playerOne-roll").click(function(event) {
-    playerOne.roll = throwdice();
-    $("#firstPlayerRoll").text(firstPlayer.roll);
-    playerOne.rollone();
-    $("#timesTotalOne").text(firstPlayer.trialPoints);
-  });
-
-  $("button#secondPlayer").click(function(event) {
-    playerTwo.roll = throwdice();
-    $("#secondPlayerRoll").text(secondPlayer.roll);
-    playerTwo.rollone();
-    $("#timesTotalTwo").text(secondPlayer.trialPoints);
-  });
-
-  $("button#firstPlayerHold").click(function(event) {
-    playerOne.hold();
-    $("#totalPointsOne").text(player1.totalscore);
-    $("#timesTotalOne").empty();
-    $("#diceRollTwo").empty();
-    playerOne.winnerCheck();
-  });
-
-  $("button#player2-hold").click(function(event) {
-    playerTwo.hold();
-    $("#totalPointsOne").text(player2.totalscore);
-    $("#timesTotalTwo").empty();
-    $("#timesRollTwo").empty();
-    playerTwo.winnerCheck();
-  });
 
 });
